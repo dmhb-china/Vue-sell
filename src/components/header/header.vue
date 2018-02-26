c168a07b6af68bbb3029e4c4bddc90d277ff1a9d
大漠汉北 <13510830600@139.com>
2018-02-26 23:11:55 +0800

    update data

--- src/components/header/header.vue
+++ src/components/header/header.vue
@@ -28,7 +28,8 @@
       <div v-show="detailShow" class="detail">
         <div class="detail-wrapper clearfix">
           <div class="detail-main">
-            {{seller.bulletin}}
+            <h1 class="name">{{seller.name}}</h1>
+            <star :size="48" :score="seller.score"></star>
           </div>
         </div>
         <div class="detail-close">
@@ -39,6 +40,7 @@
 </template>
 
 <script type="text/ecmascript-6">
+  import star from 'components/star/star';
   export default {
     props: {
       seller: {
@@ -47,7 +49,7 @@
     },
     data() {
       return {
-        detailShow: false
+        detailShow: true
       };
     },
     methods: {
@@ -60,6 +62,9 @@
     },
     created() {
       this.classMap = ['decrease', 'discount', 'special', 'invoice', 'guarantee'];
+    },
+    components: {
+      star
     }
   };
 </script>
@@ -196,10 +201,17 @@
     overflow: auto
     background: rgba(7,17,27,0.8)
     .detail-wrapper
+      width: 100%
       min-height: 100%
       .detail-main
         margin-top: 64px
         padding-bottom: 64px
+        .name
+          line-height: 16px
+          font-size: 16px
+          font-weight: 700
+          color: rgb(255,255,255)
+          text-align: center
     .detail-close
       box-sizing: border-box
       position: relative
